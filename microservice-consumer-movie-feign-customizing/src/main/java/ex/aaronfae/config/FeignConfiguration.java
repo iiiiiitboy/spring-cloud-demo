5fdebac2c8dfc94527d597e352db35d56e5b7265
AaronFae <AaronFae@163.com>
2018-07-08 02:30:36 +0800

    add feignLoggerLevel

--- microservice-consumer-movie-feign-customizing/src/main/java/ex/aaronfae/config/FeignConfiguration.java
+++ microservice-consumer-movie-feign-customizing/src/main/java/ex/aaronfae/config/FeignConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import feign.Contract;
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 
 @Configuration
@@ -20,12 +21,22 @@ public class FeignConfiguration {
 	}
 
 	/**
-	 * 配置Eureka认证信息（如果Eureka中配置了认证，该Bean可以在请求Eureka apps信息时通过认证）
+	 * 配置Feign请求Eureka的信息是需要的认证（如果Eureka中配置了认证，该Bean可以在请求Eureka apps信息时通过认证）
 	 * 
 	 * @return
 	 */
 	@Bean
 	public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
 		return new BasicAuthRequestInterceptor("af", "123456");
+	}
+
+	/**
+	 * 配置Feign的日志级别
+	 * 
+	 * @return
+	 */
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
