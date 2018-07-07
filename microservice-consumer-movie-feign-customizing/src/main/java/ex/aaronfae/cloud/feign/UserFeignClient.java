75ae9682a6ba2d960b4610006337e9c1461ad804
AaronFae <AaronFae@163.com>
2018-07-07 17:50:37 +0800

    add microservice-consumer-movie-feign-customizing module

---
+++ microservice-consumer-movie-feign-customizing/src/main/java/ex/aaronfae/cloud/feign/UserFeignClient.java
@@ -0,0 +1,15 @@
+package ex.aaronfae.cloud.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+
+import ex.aaronfae.config.FeignConfiguration;
+import ex.aaronfae.cloud.entity.User;
+import feign.Param;
+import feign.RequestLine;
+
+@FeignClient(value = "microservice-provider-user", configuration = FeignConfiguration.class)
+public interface UserFeignClient {
+
+	@RequestLine(value = "GET /user/{id}")
+	public User findOneById(@Param("id") Long id);
+}
---
+++ microservice-consumer-movie-feign-customizing/src/main/java/ex/aaronfae/config/FeignConfiguration.java
@@ -0,0 +1,15 @@
+package ex.aaronfae.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.Contract;
+
+@Configuration
+public class FeignConfiguration {
+
+	@Bean
+	public Contract feignContract() {
+		return new feign.Contract.Default();
+	}
+}
