ef39a983a5643f3cce9632724223b2c6e28e41fd
AaronFae <AaronFae@163.com>
2018-07-14 15:34:44 +0800

    add microservice-discovery-eureka-high-availability module

---
+++ microservice-discovery-eureka-high-availability/src/main/java/ex/aaronfae/cloud/MicroserviceDiscoveryEurekaHighAvailabilityApplication.java
@@ -0,0 +1,14 @@
+package ex.aaronfae.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceDiscoveryEurekaHighAvailabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDiscoveryEurekaHighAvailabilityApplication.class, args);
+	}
+}
---
+++ microservice-discovery-eureka-high-availability/src/test/java/ex/aaronfae/cloud/MicroserviceDiscoveryEurekaHighAvailabilityApplicationTests.java
@@ -0,0 +1,16 @@
+package ex.aaronfae.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceDiscoveryEurekaHighAvailabilityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
